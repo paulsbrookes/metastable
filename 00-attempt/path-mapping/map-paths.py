c48ea739108161880fabc512a73e2e203b02cb86
Paul Brookes <paul@turintech.ai>
2024-07-13 19:19:09 +0100

    chore: remove old notebooks which were used to modularise path mapping

---
+++ 00-attempt/path-mapping/map-paths.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+
+from metastable.state import FixedPointMap, FixedPointType
+from metastable.paths.map import map_switching_paths, IndexPair
+
+
+map_path = Path("/home/paul/Projects/keldysh/metastable/00-attempt/map.npz")
+output_path = Path("/home/paul/Projects/keldysh/metastable/00-attempt/output")
+
+# Check if the directory exists
+if output_path.exists():
+    raise FileExistsError(f"The directory {output_path} already exists.")
+else:
+    # Create the directory
+    output_path.mkdir(parents=True)
+
+# Load the map of fixed points
+fixed_point_map = FixedPointMap.load(map_path)
+
+
+# Build the queue of IndexPairs whose paths we will map
+epsilon_idx = 360
+seed_kappa_idx = 230
+index_list = [
+    IndexPair(epsilon_idx, seed_kappa_idx),
+    IndexPair(epsilon_idx, seed_kappa_idx - 1),
+]
+
+# Begin mapping switching paths
+results = map_switching_paths(
+    fixed_point_map, index_list, output_path, endpoint_type=FixedPointType.BRIGHT
+)

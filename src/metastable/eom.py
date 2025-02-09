6e8849d960fa1a60d5b1e59e511e23e558ac666c
Paul Brookes <paul@turintech.ai>
2025-02-09 10:26:26 +0000

    chore: change notation from x1, x2, p1, p2 to xc, pc, xq, pq

--- src/metastable/eom.py
+++ src/metastable/eom.py
@@ -10,36 +10,36 @@ class Params(BaseModel):
 
 
 class EOM:
-    x1, x2, p1, p2 = sympy.symbols("x1 x2 p1 p2")
-    sym = (x1, x2, p1, p2)
+    xc, pc, xq, pq = sympy.symbols("xc pc xq pq")
+    sym = (xc, pc, xq, pq)
 
     def __init__(self, params: Params):
         self.params: Params = params
 
         self.H = (
-            self.params.kappa * (self.p1**2 + self.p2**2)
-            - self.params.kappa * (self.x1 * self.p1 + self.x2 * self.p2)
-            - self.params.delta * (self.x1 * self.p2 - self.x2 * self.p1)
+            self.params.kappa * (self.xq**2 + self.pq**2)
+            - self.params.kappa * (self.xc * self.xq + self.pc * self.pq)
+            - self.params.delta * (self.xc * self.pq - self.pc * self.xq)
             - (self.params.chi / 2)
-            * (self.x1**2 + self.x2**2 - self.p1**2 - self.p2**2)
-            * (self.x1 * self.p2 - self.x2 * self.p1)
-            - 2 * self.params.epsilon * self.p1
+            * (self.xc**2 + self.pc**2 - self.xq**2 - self.pq**2)
+            * (self.xc * self.pq - self.pc * self.xq)
+            - 2 * self.params.epsilon * self.xq
         )
 
-        dx1_over_dt = sympy.diff(self.H, self.p1)
-        dx2_over_dt = sympy.diff(self.H, self.p2)
-        dp1_over_dt = -sympy.diff(self.H, self.x1)
-        dp2_over_dt = -sympy.diff(self.H, self.x2)
+        dxc_over_dt = sympy.diff(self.H, self.xq)
+        dpc_over_dt = sympy.diff(self.H, self.pq)
+        dxq_over_dt = -sympy.diff(self.H, self.xc)
+        dpq_over_dt = -sympy.diff(self.H, self.pc)
 
         self.y_dot_expr = sympy.Matrix(
-            [dx1_over_dt, dx2_over_dt, dp1_over_dt, dp2_over_dt]
+            [dxc_over_dt, dpc_over_dt, dxq_over_dt, dpq_over_dt]
         )
         self.jacobian_expr = self.y_dot_expr.jacobian(self.sym)
         self.y_dot_func = lambda y: sympy.lambdify(self.sym, self.y_dot_expr)(*y)[:, 0]
         self.jacobian_func = lambda y: sympy.lambdify(self.sym, self.jacobian_expr)(*y)
 
         self.y_dot_classical_expr = sympy.Matrix(
-            self.y_dot_expr.subs([(self.p1, 0), (self.p2, 0)])[0:2]
+            self.y_dot_expr.subs([(self.xq, 0), (self.pq, 0)])[0:2]
         )
         self.jacobian_classical_expr = self.y_dot_classical_expr.jacobian(self.sym[:2])
         self.y_dot_classical_func = lambda y: sympy.lambdify(
